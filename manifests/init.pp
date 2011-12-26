5b6185a782ff4aaa233d2dfb552432e12ff6b751
Jan Vansteenkiste <jan@vstone.eu>
2011-12-26 17:11:27 +0100

    Add class to import so pluginsync can do his magic

---
+++ manifests/init.pp
@@ -0,0 +1,6 @@
+# == Class: puppetlabs-create_resources
+#
+# This module contains a backport for the function create_resources
+# for puppet < 2.7.x. This class is an easy way to ensure the
+# plugin gets synced. For this, you will need pluginsync enabled.
+class puppetlabs-create_resources {}
